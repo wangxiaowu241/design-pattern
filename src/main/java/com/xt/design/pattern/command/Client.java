646310da0c84aa55f7c826f61b18684c3c1a06c6
wangxiaowu241 <wanggxiaowu241@126.com>
2019-05-13 17:19:58 +0800

    命令模式

--- src/main/java/com/xt/design/pattern/command/Client.java
+++ src/main/java/com/xt/design/pattern/command/Client.java
@@ -4,13 +4,12 @@ package com.xt.design.pattern.command;
 /**
  * 命令模式test
  * 命令模式：将请求的调用方和接收方分离，从而解耦。调用方无需关心是谁处理了这个命令，也无需关心如何处理。
- *
+ * <p>
  * Command：声明了所有具体命令类的抽象接口
  * 具体命令（ConcreteCommand）：定义接受者和执行者之间的弱耦合，实现execute方法，负责处理调用方的具体操作。
  * Client：创建一个具体命令执行者对象，并确定其接收者
  * Invoker：负责调用命令对象执行请求。
  * Receiver：负责具体实施和执行一个请求。任何一个类都可以成为接收者，实施和执行请求的方法叫做行动方法。
- *
  */
 public class Client {
 
@@ -21,7 +20,5 @@ public class Client {
         invoker.setCommand(concreteCommand);
         invoker.invoke();
 
-        invoker.setCommand(new SellCommand(receiver));
-        invoker.invoke();
     }
 }
